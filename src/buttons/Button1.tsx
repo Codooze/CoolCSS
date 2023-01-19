7c57e361414d3f141fc92047bfe95aff4c98b3eb
JACE <jeysonm021@gmail.com>
2023-01-19 17:22:27 -0500

    fixes

--- src/buttons/Button1.tsx
+++ src/buttons/Button1.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./button1.scss";
 
 interface Button1Props {
@@ -5,11 +6,23 @@ interface Button1Props {
 }
 
 export const Button1 = ({ handleClick }: Button1Props) => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setViewportWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className="bton1__container">
-      <a href="#" className="button1" onClick={handleClick}>
-        <span className="bton_1"> Change View</span>
-      </a>
-    </div>
+    <>
+      {viewportWidth > 605 && (
+        <div className="bton1__container">
+          <a href="#" className="button1" onClick={handleClick}>
+            <span className="bton_1"> Change View</span>
+          </a>
+        </div>
+      )}
+    </>
   );
 };
