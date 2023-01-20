b88381d8b9e75b27fee712c9443c3a7613935ba9
JACE <jeysonm021@gmail.com>
2023-01-20 08:35:57 -0500

    content

--- src/netflixCopy/Netflix.tsx
+++ src/netflixCopy/Netflix.tsx
@@ -1,5 +1,6 @@
 import LOGO from "../assets/netflix/logo.png";
 import BG from "../assets/netflix/bg.jpg";
+import NAME from "../assets/netflix/name.png";
 import "./netflix.scss";
 
 export const Netflix = () => {
@@ -33,6 +34,31 @@ export const Netflix = () => {
       </header>
       <div className="banner">
         <img src={BG} alt="" className="bg" />
+        <div className="content">
+          <img src={NAME} alt="" className="movieTitle" />
+          <h4>
+            <span>2020</span>
+            <span>
+              <i>12+</i>
+            </span>
+            <span>1h 55min</span>
+            <span>Action</span>
+          </h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae
+            nostrum dignissimos rerum! Magni, velit deleniti, culpa quas debitis
+            aut a consequuntur provident exercitationem quos quibusdam quasi
+            sed, hic alias pariatur!
+          </p>
+          <div className="buttons">
+            <a href="#">
+              <i className="fa fa-play" aria-hidden="true"></i> Play
+            </a>
+            <a href="#">
+              <i className="fa fa-plus" aria-hidden="true"></i>My List
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
